1e81a6a6cd8d2ec501056905a3453c928a8f0119
janardhan215 <gsaijanardhan@gmail.com>
2023-01-22 01:24:14 +0530

    version1

---
+++ src/main/java/com/jana/movies/Review.java
@@ -0,0 +1,24 @@
+package com.jana.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    private ObjectId id;
+
+    public Review(String body) {
+        this.body = body;
+    }
+
+    private String body;
+
+
+}
---
+++ src/test/java/com/jana/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jana.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
